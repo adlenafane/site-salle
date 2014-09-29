48540dc607d71e4fec7a25147f873e5c1abff8b0
Adlen Afane <adlen.afane@gmail.com>
2014-09-29 20:09:47 +0200

    Added default page for the gym place

---
+++ public/modules/salle/config/salle.client.routes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//Setting up route
+angular.module('salle').config(['$stateProvider',
+	function($stateProvider) {
+		// Salle state routing
+		$stateProvider.
+		state('salle', {
+			url: '/salle',
+			templateUrl: 'modules/salle/views/salle.client.view.html'
+		});
+	}
+]);
---
+++ public/modules/salle/controllers/salle.client.controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('salle').controller('SalleController', ['$scope',
+	function($scope) {
+		return;
+	}
+]);
