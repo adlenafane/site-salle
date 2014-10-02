40028e6f20604a94991f00df417e0f39b308512c
Adlen Afane <adlen.afane@gmail.com>
2014-10-02 23:49:52 +0200

    Added carousel

--- public/modules/salle/controllers/salle.client.controller.js
+++ public/modules/salle/controllers/salle.client.controller.js
@@ -2,6 +2,56 @@
 
 angular.module('salle').controller('SalleController', ['$scope',
 	function($scope) {
-		return;
+    $scope.carouselInterval = 5000;
+
+    $scope.slidesCardio = [
+      {
+        image: '/modules/salle/img/cardio.jpg'
+      }
+    ];
+
+    $scope.slidesLibre = [
+      {
+        image: '/modules/salle/img/libre.jpg'
+      }
+    ];
+
+    $scope.slidesGuide = [
+      {
+        image: '/modules/salle/img/guide.jpg'
+      }
+    ];
+
+    $scope.slidesClasses = [
+      { image: '/modules/salle/img/classes.jpg' },
+      { image: '/modules/salle/img/classes-2.jpg' },
+      { image: '/modules/salle/img/classes-3.jpg' },
+      { image: '/modules/salle/img/classes-4.jpg' },
+      { image: '/modules/salle/img/classes-5.jpg' }
+    ];
+
+    $scope.slidesBiking = [
+      {
+        image: '/modules/salle/img/biking.jpg'
+      }
+    ];
+
+    $scope.slidesFreedom = [
+      {
+        image: '/modules/salle/img/freedom.jpg'
+      }
+    ];
+
+    $scope.slidesTrx = [
+      {
+        image: '/modules/salle/img/trx.jpg'
+      }
+    ];
+
+    $scope.slidesRest = [
+      {
+        image: '/modules/salle/img/rest.jpg'
+      }
+    ];
 	}
 ]);
