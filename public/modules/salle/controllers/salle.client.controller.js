abbaab2563c5b73881ea40b07e411d40ed7b48a3
Adlen Afane <adlen.afane@gmail.com>
2014-10-03 22:16:21 +0200

    Set only one carousel

--- public/modules/salle/controllers/salle.client.controller.js
+++ public/modules/salle/controllers/salle.client.controller.js
@@ -4,54 +4,33 @@ angular.module('salle').controller('SalleController', ['$scope',
 	function($scope) {
     $scope.carouselInterval = 5000;
 
-    $scope.slidesCardio = [
-      {
-        image: '/modules/salle/img/cardio.jpg'
-      }
-    ];
-
-    $scope.slidesLibre = [
-      {
-        image: '/modules/salle/img/libre.jpg'
-      }
-    ];
-
-    $scope.slidesGuide = [
-      {
-        image: '/modules/salle/img/guide.jpg'
-      }
-    ];
-
-    $scope.slidesClasses = [
+    $scope.slides = [
+      { image: '/modules/salle/img/cardio.jpg' },
+      { image: '/modules/salle/img/libre.jpg' },
+      { image: '/modules/salle/img/guide.jpg' },
       { image: '/modules/salle/img/classes.jpg' },
       { image: '/modules/salle/img/classes-2.jpg' },
       { image: '/modules/salle/img/classes-3.jpg' },
       { image: '/modules/salle/img/classes-4.jpg' },
-      { image: '/modules/salle/img/classes-5.jpg' }
-    ];
-
-    $scope.slidesBiking = [
-      {
-        image: '/modules/salle/img/biking.jpg'
-      }
-    ];
-
-    $scope.slidesFreedom = [
-      {
-        image: '/modules/salle/img/freedom.jpg'
-      }
-    ];
-
-    $scope.slidesTrx = [
-      {
-        image: '/modules/salle/img/trx.jpg'
-      }
-    ];
-
-    $scope.slidesRest = [
-      {
-        image: '/modules/salle/img/rest.jpg'
-      }
+      { image: '/modules/salle/img/classes-5.jpg' },
+      { image: '/modules/salle/img/biking.jpg' },
+      { image: '/modules/salle/img/freedom.jpg' },
+      { image: '/modules/salle/img/trx.jpg' },
+      { image: '/modules/salle/img/rest.jpg' }
     ];
 	}
+]);
+
+/** Fix animation for UI Bootstrap Carousel **/
+angular.module('salle').directive('disableAnimation', ['$animate',
+  function($animate){
+    return {
+      restrict: 'A',
+      link: function($scope, $element, $attrs){
+        $attrs.$observe('disableAnimation', function(value){
+          $animate.enabled(!value, $element);
+        });
+      }
+    }
+  }
 ]);
