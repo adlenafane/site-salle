d28983131a0616668b792535588b2bf5a4445aab
Adlen Afane <adlen.afane@gmail.com>
2014-10-04 22:45:22 +0200

    Added smooth scroll and increase size

--- config/env/all.js
+++ config/env/all.js
@@ -16,6 +16,7 @@ module.exports = {
                 'public/dist/style.css'
             ],
             js: [
+                'public/lib/jquery/dist/jquery.js',
                 'public/lib/angular/angular.js',
                 'public/lib/angular-resource/angular-resource.js',
                 'public/lib/angular-cookies/angular-cookies.js',
--- public/modules/salle/controllers/salle.client.controller.js
+++ public/modules/salle/controllers/salle.client.controller.js
@@ -21,6 +21,27 @@ angular.module('salle').controller('SalleController', ['$scope',
 	}
 ]);
 
+angular.module('salle').directive('roomRow', [ '$window',
+  function($window) {
+    return {
+      restrict: 'EA',
+      transclude: true,
+      scope: {
+        id: '@'
+      },
+      template: '<div ng-transclude="" ng-click="toggle();"></div>',
+      link: function($scope) {
+        var $ = $window.$;
+        $scope.toggle = function() {
+          angular.element( 'html, body' ).animate( {
+            scrollTop: $('#' + $scope.id).offset().top
+          }, 1000 );
+        };
+      }
+    };
+  }
+]);
+
 /** Fix animation for UI Bootstrap Carousel **/
 angular.module('salle').directive('disableAnimation', ['$animate',
   function($animate){
