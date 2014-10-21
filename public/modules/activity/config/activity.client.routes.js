eca6776d1b8f4f4b64611b95b052b4f3cc4546dd
Adlen Afane <adlen.afane@gmail.com>
2014-10-21 22:49:19 +0200

    Added path to category details

--- public/modules/activity/config/activity.client.routes.js
+++ public/modules/activity/config/activity.client.routes.js
@@ -5,9 +5,13 @@ angular.module('activity').config(['$stateProvider',
 	function($stateProvider) {
 		// Activity state routing
 		$stateProvider.
-		state('activity', {
-			url: '/activites',
-			templateUrl: 'modules/activity/views/activity.client.view.html'
+    state('activity', {
+      url: '/activites',
+      templateUrl: 'modules/activity/views/activity.client.view.html'
+    })
+    .state('activity-details', {
+			url: '/activites/:category',
+			templateUrl: 'modules/activity/views/activity-details.client.view.html'
 		});
 	}
 ]);
--- public/modules/planning/controllers/planning.client.controller.js
+++ public/modules/planning/controllers/planning.client.controller.js
@@ -18,6 +18,6 @@ angular.module('planning').controller('PlanningController', ['$scope',
           }
         ]
       }
-    ]
+    ];
 	}
 ]);
