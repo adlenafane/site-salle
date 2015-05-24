b0054a8ee236d89fc758f0801a444eaaf41359aa
Adlen Afane <adlen.afane@gmail.com>
2015-05-24 10:38:06 +0200

    Updated pics

--- public/dist/application.js
+++ public/dist/application.js
@@ -1040,13 +1040,12 @@ angular.module('salle').controller('SalleController', [
     $scope.carouselInterval = 5000;
     $scope.slides = [
       { image: '/modules/salle/img/cardio-header.jpg' },
-      { image: '/modules/salle/img/1.jpg' },
-      { image: '/modules/salle/img/2.jpg' },
       { image: '/modules/salle/img/guide-header.jpg' },
       { image: '/modules/salle/img/9.jpg' },
       { image: '/modules/salle/img/3.jpg' },
       { image: '/modules/salle/img/classes-header.jpg' },
-      { image: '/modules/salle/img/4.jpg' },
+      { image: '/modules/salle/img/11.jpg' },
+      { image: '/modules/salle/img/12.jpg' },
       { image: '/modules/salle/img/libre-header.jpg' },
       { image: '/modules/salle/img/6.jpg' },
       { image: '/modules/salle/img/7.jpg' },
