87c2970fd83e1bdc744f7e9450ac6916ea0d3176
Adlen Afane <adlen.afane@gmail.com>
2015-05-24 23:34:14 +0200

    Generate planning image

--- public/dist/application.js
+++ public/dist/application.js
@@ -902,7 +902,8 @@ angular.module('planning').controller('PlanningController', [
   '$scope',
   '$state',
   '$http',
-  function ($scope, $state, $http) {
+  '$window',
+  function ($scope, $state, $http, $window) {
     $scope.search = {};
     $scope.coaches = [];
     $scope.categories = [];
@@ -932,6 +933,38 @@ angular.module('planning').controller('PlanningController', [
         }
       }
     }, true);
+    var getCurrentDate = function () {
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth() + 1;
+      //January is 0!
+      var yyyy = today.getFullYear();
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+      today = yyyy + '-' + mm + '-' + dd;
+      return today;
+    };
+    $scope.downloadPlanning = function () {
+      // Reinit search view
+      var tmpSearch = $scope.search;
+      $scope.search = {};
+      $window.html2canvas($window.$('.planning.table'), {
+        onrendered: function (canvas) {
+          var img = canvas.toDataURL('image/png').replace('image/png', 'image/octet-stream');
+          var downloadLink = document.createElement('a');
+          downloadLink.href = img;
+          downloadLink.download = getCurrentDate() + '-MagicFormFAR-planning.png';
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+          $scope.search = tmpSearch;
+        }
+      });
+    };
   }
 ]);'use strict';
 //Setting up route
